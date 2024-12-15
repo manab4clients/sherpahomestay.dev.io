2950d01590e1ccc26b6764d3460cf8e31826adf8
manab4clients <manabendra.jha+clientonly@citytask.com>
2024-12-15 13:20:36 +0530

    Google Map Update

--- lib/init.php
+++ lib/init.php
@@ -9,7 +9,10 @@ $phone = '(+91) 877 774 1034';
 $whatsapp = $phone;
 //$email = 'anirban.udyogi@gmail.com';
 $email = 'jhandisherpahomestay@gmail.com ';
-$google_map = 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3554.364203559265!2d88.64320487544315!3d27.018655176584154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjfCsDAxJzA3LjIiTiA4OMKwMzgnNDQuOCJF!5e0!3m2!1sen!2sin!4v1732305436543!5m2!1sen!2sin';
+//$google_map = 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3554.364203559265!2d88.64320487544315!3d27.018655176584154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjfCsDAxJzA3LjIiTiA4OMKwMzgnNDQuOCJF!5e0!3m2!1sen!2sin!4v1732305436543!5m2!1sen!2sin';
+
+$google_map = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3554.3658351335594!2d88.64580869999999!3d27.0186036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e40526e9185f1b%3A0xed9a8364ce8d9598!2sSherpa%20homestay!5e0!3m2!1sen!2sin!4v1734248855222!5m2!1sen!2sin';
+
 $about_youtube = 'https://youtu.be/0wO7GTjw3Us?si=XPVazL1TvYQqnWM7';
 
 $facebook = 'https://www.facebook.com/profile.php?id=61568426175581';
